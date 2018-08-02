36a8a09bf542c1bdfcdbb42ce811a9a3ddfebd42
Sridatta <sridatta.pasumarthy@oracle.com>
2018-08-02 16:02:55 +0530

    Redis CRUD Repository

--- src/main/java/com/example/redis/RedisApplication.java
+++ src/main/java/com/example/redis/RedisApplication.java
@@ -1,17 +1,28 @@
 package com.example.redis;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Reference;
 import org.springframework.data.geo.Circle;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.GeoResults;
 import org.springframework.data.geo.Point;
 import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.GeoOperations;
+import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.index.Indexed;
+import org.springframework.data.repository.CrudRepository;
+
+import java.io.Serializable;
+import java.util.*;
 
 @Log
 @SpringBootApplication
@@ -43,7 +54,77 @@ public class RedisApplication {
 		});
 	}
 
+	@Bean
+	ApplicationRunner repositories(OrderRepository orderRepository, LineItemRepository lineItemRepository) {
+		return titledRunner("repositories", args -> {
+
+			Long orderId = generateId();
+
+			List<LineItem> itemList = Arrays.asList(
+				new LineItem(orderId, generateId(), "plunger"),
+				new LineItem(orderId, generateId(), "soup"),
+				new LineItem(orderId, generateId(), "coffee mug")
+			);
+			itemList
+				.stream()
+				.map(lineItemRepository::save)
+				.forEach(li -> log.info(li.toString()));
+
+			Order order = new Order(orderId, new Date(), itemList);
+			orderRepository.save(order);
+
+			Collection<Order> found = orderRepository.findByWhen(order.getWhen());
+			found.forEach(o -> log.info("found: " + o.toString()));
+		});
+	}
+
+	private Long generateId() {
+		long tmp = new Random().nextLong();
+		return Math.max(tmp, tmp* -1);
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
 	}
+}
+
+interface OrderRepository extends CrudRepository<Order, Long> {
+
+	Collection<Order> findByWhen(Date d);
+}
+
+interface LineItemRepository extends CrudRepository<LineItem, Long> {
+
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash("orders")
+class Order implements Serializable {
+	@Id
+	private Long id;
+
+	@Indexed
+	private Date when;
+
+	@Reference
+	private List<LineItem> lineItems;
+
+}
+
+@RedisHash("lineItems")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class LineItem implements Serializable {
+
+	@Indexed
+	private Long orderId;
+
+	@Id
+	private Long id;
+
+	private String description;
 }
